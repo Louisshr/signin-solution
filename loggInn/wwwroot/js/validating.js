b9e352141323b13585bf3740da7b90756311452f
LouisVitenskap <70730534+LouisVitenskap@users.noreply.github.com>
2023-06-14 08:47:35 +0200

    updated the validation on frontend

--- loggInn/wwwroot/js/validating.js
+++ loggInn/wwwroot/js/validating.js
@@ -15,7 +15,7 @@ function validatePassword(password) {
     const regexp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
     const ok = regexp.test(password);
     if (!ok) {
-        $("#invalidPassword").html("password must consist of at least 6 signs, of which one is a letter and another a number");
+        $("#    ).html("password must consist of at least 6 signs, of which one is a letter and another a number");
         return false;
     }
     else {
